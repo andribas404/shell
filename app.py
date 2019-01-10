68c0514efdc4b478fdb50cfdee1f1a3fa1ed7433
andribas <andribas404@gmail.com>
2019-01-10 18:58:36 +0500

    added peewee

---
+++ app.py
@@ -0,0 +1,14 @@
+import datetime
+import os
+import re
+
+from peewee import *
+
+db = SqliteDatabase('people.db')
+
+class Person(Model):
+    name = CharField()
+    birthday = DateField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
