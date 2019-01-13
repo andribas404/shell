dbb7d96f383393b7af3d289d7e60484f4ab8b977
andribas <andribas404@gmail.com>
2019-01-14 02:15:23 +0500

    env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-process.env.NODE_ENV = 'production';
+var NODE_ENV = 'production';
 let baseUrl = ''
-if (process.env.NODE_ENV === 'production') {
+if (NODE_ENV === 'production') {
    baseUrl = 'https://person-list.herokuapp.com/api'
 } else {
    baseUrl = 'http://localhost:5000/api'
